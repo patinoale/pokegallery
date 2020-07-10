cbc31a8070a6e13c5c46eb4c01b4debe36da02cc
Alejandra Patino <ale@alejandras-mbp.home>
2020-07-10 16:46:18 -0700

    finished working app

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+//jQuery IIFE
+$(function() {
+
+
 /*----- constants -----*/
 const baseURL = 'https://pokeapi.co/api/v2/pokemon';
 
@@ -5,34 +9,81 @@ const baseURL = 'https://pokeapi.co/api/v2/pokemon';
 
 /*----- app's state (variables) -----*/
 
-let pokemonData, pokemonFetail;
+let pokemonData, pokemonDetails;
 
 
 /*----- cached element references -----*/
 
-const $ulEl = $(".collection")
-
+const $ulEl = $(".collection");
+const $imgEl =$('.modal-content img');
+const $name =$('#name');
+const $moves = $('#moves');
+const $abilities = $('#abilities');
+const $height = $('#height');
+const $modal = $('.modal');
 
 /*----- event listeners -----*/
-$ulEl.on('click', handleClick);
+$ulEl.on('click', 'span', handleClick);
+
 
 /*----- functions -----*/
+$modal.modal(); // initialize modal
+const instance = M.Modal.getInstance($modal);
 
 function handleClick(event){ 
-console.log(event);
+    getPokemon(event.target.dataset.url, true);
 };
 
 //make data available as soon as the app loads 
 getPokemon();
 
-function getPokemon() {
-    $.ajax(baseURL)
+function getPokemon(detailURL, isDetail = false) {
+    const url = detailURL || baseURL;
+
+    $.ajax(url)
     .then(
+        
         (data) => {
+            if(!isDetail) {
             pokemonData = data.results;
+            render(); //programtically render the html
+            }else{
+                pokemonDetails = data;
+                render(true);
+            }
         },
         (error) => {
             console.log('error is', error);
         }
     );
 }
+
+function generateHTML() {
+    return pokemonData.map(function(pokemon) {
+        return ` 
+        <li class="collection-item red-text">
+            <div style="text-transform: capitalize;">${pokemon.name}
+                <span data-url="${pokemon.url}" class="secondary-content blue-text">
+        Details</span>
+        </div>
+        </li>`;
+    });
+}
+
+function render(isDetail = false) {
+    if(!isDetail){
+    const html = generateHTML().join("")
+    $ulEl.html(html)
+}else {
+    //produce the modal
+    $imgEl.attr('src', pokemonDetails.sprites.front_default);
+    $imgEl.attr('alt',pokemonDetails.name);
+    $name.text(pokemonDetails.name);
+    $height.text("Height: " + pokemonDetails.height);
+    $moves.text("Number of moves: " +pokemonDetails.moves.length);
+    $abilities.text("Number of abilities: " +pokemonDetails.abilities.length);
+    //open the modal 
+    instance.open();
+    }
+}
+});
